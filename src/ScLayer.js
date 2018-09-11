b4c34e885aca8b3937ab666ce14da180a5d261a0
army8735 <army8735@qq.com>
2018-09-11 11:59:23 +0800

    init

---
+++ src/ScLayer.js
@@ -0,0 +1,8 @@
+'use strict';
+
+class ScLayer {
+  constructor() {}
+  parse() {}
+}
+
+export default ScLayer;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import sketch from 'sketch';
+import { Document } from 'sketch/dom';
+import UI from 'sketch/ui';
 
-export default function(context) {
-  let selection = context.selection;
-    if(selection.count() === 0) {
-    sketch.UI.alert('Error', 'At lease a layer myst be selected!');
+import ScLayer from './ScLayer';
+
+export default function() {
+  let document = Document.getSelectedDocument();
+  let selection = document.selectedLayers;
+  if(selection.isEmpty) {
+    UI.alert('Warn', 'At lease one layer must be selected!');
     return;
   }
-}
+  let list = [];
+  selection.map((item) => {
+    let scLayer = new ScLayer(item);
+    scLayer.parse();
+    list.push(scLayer);
+  });
+  let options = ['Desktop', 'Documents', 'Downloads', 'Custom'];
+  let sel = UI.getSelectionFromUser(
+    "Please choose your output directory:",
+    options
+  );
+  let ok = sel[2];
+  let value = options[sel[1]];
+  if(ok) {
+    let output = `~/${value}/sketch2code`;
+    console.log(output);
+  }
+};
